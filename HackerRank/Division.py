a779f698b0584e1d3f974122c7de5b2829f56d6b
RitaHuang1998 <133912948+RitaHuang1998@users.noreply.github.com>
2023-05-23 02:25:17 +0800

    Create Division.py

---
+++ HackerRank/Division.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    # Integer Division
+    # Quotient
+    IntegerDivisionQuotient = a//b
+    # Remainder
+    IntegerDivisionRemainder = a%b
+    # Float Division
+    FloatDivision = a/b
+    print(str(IntegerDivisionQuotient)+'\n'+str(FloatDivision))
