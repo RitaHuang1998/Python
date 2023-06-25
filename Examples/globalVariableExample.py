faeddcbd18f7af442f022a685cc7e1cfb56e6894
Rita <133912948+RitaHuang1998@users.noreply.github.com>
2023-06-26 01:33:07 +0800

    Upload Global Variable Example

---
+++ Examples/globalVariableExample.py
@@ -0,0 +1,26 @@
+# Global Variable Example
+
+# Global name = Rita
+name = 'Rita'
+
+def main():
+  example()
+
+
+
+def example():
+    # 👇️ mark global
+    global name # 👉️ error: local variable 'name' referenced before assignment
+
+    #Global name = Rita
+    print(name) # 👉️ 'Rita'
+
+    name = 'Rong'
+
+    #Global name = Rong
+    print(name) # 👉️ 'Rong'
+
+# Run first
+if __name__ == "__main__":
+  # Call "def main()"
+  main()
